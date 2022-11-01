88020af8c24d72b22cee7aeb565efcd1a7780bdf
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-01 09:32:22 +0300

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int lenght = 19, width = 10;
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < lenght; j++)
+        {
+            if (i == 0 || i == width - 1 || j == 0 || j == lenght - 1) cout << "+";
+            else cout << "*";
+        }
+        cout << endl;
+    }
+    system("pause");
+}
